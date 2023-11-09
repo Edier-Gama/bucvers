ec7f8424f7f895d19769d866dd09ce9080dfd21d
Edier Gama <ediersantiago7@gmail.com>
2023-11-08 23:03:56 -0500

    re inventing register component

--- src/components/register/validateForm.jsx
+++
@@ -1,9 +0,0 @@
-const validateForm = (event, firstName, lastName, username, email, password, setDataStatus) => {
-  event.preventDefault()
-
-  if (username && email && password && firstName && lastName) {
-    setDataStatus(true)
-  }
-}
-
-export { validateForm }
